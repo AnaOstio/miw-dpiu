f5ef65ee0c8f334d7caaa9ac757f3c39fa3e709b
Ana <anafdezostio01@gmail.com>
2024-11-08 09:52:55 +0100

    fix menuaapp bar

--- frontend-wallapep/src/components/common/MenuApp.js
+++ frontend-wallapep/src/components/common/MenuApp.js
@@ -33,7 +33,7 @@ let MenuApp = ({ login, setLogin }) => {
 
     return (
         <>
-            <Row style={{ width: "100vw", margin: 0, padding: 0, backgroundColor: "#001529" }} justify="space-between">
+            <Row style={{ width: "100%", margin: 0, padding: 0, backgroundColor: "#001529" }} justify="space-between">
                 <Col span={22} style={{ padding: 0, margin: 0 }}>
                     {!login && (
                         <Menu theme="dark" mode="horizontal" style={{ width: "100%", padding: 0, margin: 0 }} items={[
@@ -45,7 +45,7 @@ let MenuApp = ({ login, setLogin }) => {
 
                     {login && (
                         <Menu theme="dark" mode="horizontal" style={{ width: "100%", padding: 0, margin: 0 }} items={[
-                            { key: "logo", label: <img src="/logo.png" width="40" height="40" style={{ display: "block", margin: "0 auto", paddingTop: "5px" }} /> },
+                            { key: "logo", label: <img src="/logo.png" width="40" height="40" style={{ display: "block", margin: "0 auto" }} /> },
                             { key: "menuProducts", icon: <ShopOutlined />, label: <Link to="/all-products/all">Products</Link> },
                             { key: "menuCreateProduct", icon: <PlusCircleOutlined />, label: <Link to="/products/create">Sell</Link> },
                             { key: "menuMyProduct", icon: <FileOutlined />, label: <Link to="/products/own">My Products</Link> },
@@ -54,8 +54,7 @@ let MenuApp = ({ login, setLogin }) => {
                         ]} />
                     )}
                 </Col>
-                <Col xs={6} sm={5} md={4} lg={3} xl={2}
-                    style={{ display: 'flex' }}>
+                <Col xs={4} sm={4} md={4} lg={3} xl={2} style={{ display: 'flex', justifyContent: 'flex-end' }}>
                     {login ? (
                         <Avatar size="large"
                                 style={{ backgroundColor: "#ff0000", verticalAlign: 'middle', marginTop: 12 }}>
